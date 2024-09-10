5dd1bf7ee78265aa831fca842e6e8918d9179387
Alessandro Murdocca <alessandro.murdocca@beije.it>
2024-09-10 17:24:44 +0200

    add talwind, font and various

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: { primary: "#0f172b", secondary: "#fea116" },
+      animation: {
+        "spin-slow": "spin 45s linear infinite",
+      },
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+        heebo: ["Heebo", "sans-serif"],
+        pacifico: ["Pacifico", "sans-serif"],
+      },
+      /* backgroundImage:{
+        heroBg: "url('/public/hero.jpg')"
+      } */
+    },
+  },
+  plugins: [
+    require("tailwindcss-animate"),
+    // ...
+  ],
+};
